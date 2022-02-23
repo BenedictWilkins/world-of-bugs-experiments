51c6b4c4c98dd92a3cf1c8cb623887d72fdd782f
Benedict Wilkins <benrjw@gmail.com>
2022-02-23 11:45:00 +0000

    fixed set to use https

--- aws/install_dpkg.sh
+++ aws/install_dpkg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt-get update
-sudo apt-get autoremove
+sudo apt update -y
+sudo apt-get update -y
+sudo apt-get autoremove -y
 
 sudo apt-get install git -y
 sudo apt-get install ffmpeg -y
--- aws/run.sh
+++ aws/run.sh
@@ -9,22 +9,16 @@ ssh ubuntu@$1 "source $DIRECTORY/waitfordpkg.sh && exit"
 ssh ubuntu@$1 "source $DIRECTORY/install_dpkg.sh $DIRECTORY && exit"
 
 # install anaconda, restarting the connection creates another shell, which is important for anaconda setup...
-ssh ubuntu@$1 "source $DIRECTORY/install_conda.sh && exit"
+# this is only needed if ec2 instance doesnt already have anaconda installed!
+#ssh ubuntu@$1 "source $DIRECTORY/install_conda.sh && exit"
+
+# setup kaggle & wandb 
+rsync -rv --inplace ~/.wandb/ ubuntu@$1:/home/ubuntu/.wandb/
+rsync -rv --inplace ~/.kaggle/ ubuntu@$1:/home/ubuntu/.kaggle/
 
 # install python packages
 ssh ubuntu@$1 "source $DIRECTORY/install_python.sh  && exit"
 
 
 
-
-# setup kaggle & wandb 
-rsync -rv --inplace ~/.wandb/ ubuntu@$1:/home/ubuntu/.wandb/
-rsync -rv --inplace ~/.kaggle/ ubuntu@$1:/home/ubuntu/.kaggle/
-
-ssh ubuntu@1 "source $DIRECTORY/wandb.sh && exit"
-
-
-
-
-
-# TODO other commands
+# TODO other commands?
