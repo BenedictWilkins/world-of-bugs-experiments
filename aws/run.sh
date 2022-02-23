504f9da7bda1ba6a9bb9fae0cc2210b00b5aa855
Benedict Wilkins <benrjw@gmail.com>
2022-02-23 12:40:38 +0000

    config

--- aws/run.sh
+++ aws/run.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
+
+# =====================
+# Use the Deep Learning Base AMI (ubuntu)
+# g3.4xlarge
+# =====================
+
 DIRECTORY="/home/ubuntu/.aws"
 # sync install files
 rsync -rv --inplace ./ ubuntu@$1:$DIRECTORY
 
 # wait for dpkg to become avaliable...
-ssh ubuntu@$1 "source $DIRECTORY/waitfordpkg.sh && exit"
+#ssh ubuntu@$1 "source $DIRECTORY/waitfordpkg.sh && exit"
 # update and install packages
-ssh ubuntu@$1 "source $DIRECTORY/install_dpkg.sh $DIRECTORY && exit"
+#ssh ubuntu@$1 "source $DIRECTORY/install_dpkg.sh $DIRECTORY && exit"
 
 # install anaconda, restarting the connection creates another shell, which is important for anaconda setup...
 # this is only needed if ec2 instance doesnt already have anaconda installed!
-#ssh ubuntu@$1 "source $DIRECTORY/install_conda.sh && exit"
+ssh ubuntu@$1 "source $DIRECTORY/install_anaconda.sh && exit"
 
 # setup kaggle & wandb 
 rsync -rv --inplace ~/.wandb/ ubuntu@$1:/home/ubuntu/.wandb/
@@ -19,6 +25,6 @@ rsync -rv --inplace ~/.kaggle/ ubuntu@$1:/home/ubuntu/.kaggle/
 # install python packages
 ssh ubuntu@$1 "source $DIRECTORY/install_python.sh  && exit"
 
+# do your thing!
+ssh ubuntu@$1 
 
-
-# TODO other commands?
