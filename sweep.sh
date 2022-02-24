0dc96da0090ee9243614273fe0c1e08da97ce4c2
Benedict Wilkins <benrjw@gmail.com>
2022-02-24 18:36:01 +0000

    updates

--- sweep.sh
+++ sweep.sh
@@ -1,5 +1,5 @@
 
-python -m world-of-bugs-experiments '++lightning.criterion._target_=world-of-bugs-experiments.module.MSESSIM' '++lightning.criterion.window_size=7' '++lightning.optimiser.lr=0.0005' '++lightning.score._target_=nn.MSELoss' '++trainer.max_epochs=5' '++lightning.model._target_=world-of-bugs-experiments.module.AEConv'
+python -m world-of-bugs-experiments '++lightning.criterion._target_=world-of-bugs-experiments.module.MSESSIM' '++lightning.criterion.window_size=7' '++lightning.optimiser.lr=0.0005' '++lightning.score._target_=torch.nn.MSELoss' '++trainer.max_epochs=5' '++lightning.model._target_=world-of-bugs-experiments.module.AEConv'
 
 #python -m world-of-bugs-experiments '++lightning.criterion._target_=world-of-bugs-experiments.module.SSIM' '++lightning.criterion.window_size=7' '++lightning.optimiser.lr=0.0005' '++trainer.max_epochs=20' '++lightning.model._target_=world-of-bugs-experiments.module.AEAlex'
 
